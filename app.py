5173db5eea18477f99838ecb58af2285f700aae2
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2023-11-16 16:45:46 -0500

    Project api ready

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 
+import traceback
 from flask import Flask, request, jsonify, render_template
 from config.bd import app, bd, ma
 from Model.Estudiante import Estudiante, EstudianteSchema
@@ -159,12 +160,9 @@ def GetDetalle():
 @app.route('/getGrupoI', methods=['GET'])
 def GetD():
     # Verifica si hay datos JSON en la solicitud
-    if not request.is_json:
-        return {"error": "La solicitud debe contener datos JSON"}, 400
-
     # Obtén el idSemillero de los datos JSON
-    idSemillero = request.json.get("idSemillero")
-    
+    idSemillero = request.args.get("idSemillero")
+    print("semillero obtenidido" +idSemillero)
     # Verifica si idSemillero está presente en la solicitud JSON
     if idSemillero is None:
         return {"error": "Se requiere el campo 'idSemillero' en la solicitud JSON"}, 400
@@ -185,13 +183,95 @@ def GetD():
             'nombre': results.nombre,
             'lider': results.lider,
             'lineaInvestigacion': results.lineaInvestigacion,
-               
-        }
+            "codigoColciencias" :results.codigoColciencias ,
+            "categoriaColciencias" : results.categoriaColciencias,
+        }   
         return jsonify(dato)
         
 
     except Exception as e:
-        return {"error": str(e)}, 500  # Devuelve un mensaje de error y un código de estado 500 en caso de excepción
+     return {"error": str(e), "traceback": traceback.format_exc()}, 500
+
+@app.route('/Detalle2', methods=['GET'])
+def GetDetalle2():
+    idSemillero = request.json["idSemillero"]
+    print(idSemillero)
+    # Verifica si idSemillero está presente en la solicitud JSON
+    if idSemillero is None:
+        return {"error": "Se requiere el campo 'idSemillero' en la solicitud JSON"}, 400
+
+    # Consulta en la entidad proyecto donde su fk de semillero sea igual a idSemillero
+    results = bd.session.query(Proyecto, Estudiante, EstudianteP, Objetivos, ResultadosOb, Semillero) \
+        .join(Proyecto, Proyecto.idSemilleroFk == Semillero.codigoSemillero, isouter=True) \
+        .join(EstudianteP, Proyecto.codProyecto == EstudianteP.codProyectoFk, isouter=True) \
+        .join(Estudiante, EstudianteP.idEstudianteFk == Estudiante.codigoE, isouter=True) \
+        .join(Objetivos, Proyecto.codProyecto == Objetivos.idProyectoOFk, isouter=True) \
+        .join(ResultadosOb, Proyecto.codProyecto == ResultadosOb.idProyectoObFk, isouter=True) \
+        .filter(Semillero.codigoSemillero == idSemillero).all()
+
+    # Listas donde se almacenarán los datos de cada entidad
+    estudiantes_list = []
+    proyectos_list = []
+    estudiantesP_list = []
+    objetivos_list = []
+    resultadosOb_list = []
+
+    for row in results:
+        proyecto = row[0] if row[0] is not None else None
+        estudiante = row[1] if row[1] is not None else None
+        estudiantep = row[2] if row[2] is not None else None
+        objetivos = row[3] if row[3] is not None else None
+        resultadosOb = row[4] if row[4] is not None else None
+
+        if proyecto:
+            proyectos_list.append({
+                "codProyecto": proyecto.codProyecto,
+                "nombreProyecto": proyecto.nombreProyecto,
+            })
+
+        if estudiante:
+            estudiantes_list.append({
+                "codigoE": estudiante.codigoE,
+                "nombre": estudiante.nombre,
+                "apellido": estudiante.apellido,
+            })
+
+        if estudiantep:
+            estudiantesP_list.append({
+                "codigoEstudianteP": estudiantep.codigoEstudianteP,
+                "codProyectoFk": estudiantep.codProyectoFk,
+                "idEstudianteFk": estudiantep.idEstudianteFk,
+            })
+
+        if objetivos:
+            objetivos_list.append({
+                "codigoOb": objetivos.codigoOb,
+                "descripcion": objetivos.descripcion,
+            })
+
+        if resultadosOb:
+            resultadosOb_list.append({
+                "codigoRO": resultadosOb.codigoRO,
+                "descripcionOb": resultadosOb.descripcionOb,
+            })
+
+    # Eliminar duplicados de las listas
+    estudiantes_list = [dict(t) for t in {tuple(d.items()) for d in estudiantes_list}]
+    proyectos_list = [dict(t) for t in {tuple(d.items()) for d in proyectos_list}]
+    estudiantesP_list = [dict(t) for t in {tuple(d.items()) for d in estudiantesP_list}]
+    objetivos_list = [dict(t) for t in {tuple(d.items()) for d in objetivos_list}]
+    resultadosOb_list = [dict(t) for t in {tuple(d.items()) for d in resultadosOb_list}]
+
+    return jsonify({
+        "Estudiantes": estudiantes_list,
+        "Proyectos": proyectos_list,
+        "EstudiantesP": estudiantesP_list,
+        "Objetivos": objetivos_list,
+        "ResultadosOb": resultadosOb_list
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9566)
+
+
+    
